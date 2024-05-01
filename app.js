80695fb02e3998059d8654a1d66f066e45acc787
Kolt Metz <kltrmtz@gmail.com>
2024-05-01 10:41:06 -0400

    Project15 Fix

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
-const indexRouter = require("./routes/index");
 
 const errorHandler = require("./middlewares/error-handler");
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+
+const indexRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
